4a5c290fca5b98aafbc5a48e0a83092a21514808
AYcrown77 <aycrown77@gmail.com>
2022-06-07 20:32:23 +0000

    0x02.C-Functions,nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @j: the parameter passed
+ * Return: j value
+ *
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		j = j * -1;
+		return (j);
+	}
+	else
+	{
+		return (j);
+	}
+}
