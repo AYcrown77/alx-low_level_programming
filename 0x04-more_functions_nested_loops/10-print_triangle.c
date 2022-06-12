c1d4306bfb1e851a2b42823c4bdd5df146b8d067
AYcrown77 <aycrown77@gmail.com>
2022-06-12 10:31:42 +0000

    0x04.C-More functions,more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a trianglar shape
+ * @size: the size of the triangle
+ * Return: Nothing.
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (i + j <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
