a3811eab1ee9b8ab52d13f9208ca0d5ab162dcea
AYcrown77 <aycrown77@gmail.com>
2022-06-15 09:20:43 +0000

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset a var to 98
+ * @n: passed argument
+ * return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	int *m;
+
+	m = n;
+	*m = 98;
+}
