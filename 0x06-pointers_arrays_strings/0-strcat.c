83e8e56dd2228dfd05a5e708baceddfd8db75183
AYcrown77 <aycrown77@gmail.com>
2022-06-16 14:10:59 +0000

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string argument passed
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+        int count = 0;
+        int i = 0;
+
+        while (s[i] != '\0')
+        {
+                count = count + 1;
+                i++;
+        }
+        return (count);
+}
+
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2, i;
+
+	len1 = _strlen(dest);
+	len2 = _strlen(src);
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+	return (dest);
+}
