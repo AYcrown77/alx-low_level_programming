6ce64822f7a0c59219467dbf424f2c280966d601
AYcrown77 <aycrown77@gmail.com>
2022-06-18 11:44:38 +0000

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string argument passed
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		count = count + 1;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * leet - function that encodes a string into 1337
+ * @s: string function passed
+ * Return: modified string (s)
+ */
+char *leet(char *s)
+{
+	int i, j, len1, len2, tmp;
+	char alp[] = {'a', 'e', 'o', 't', 'l'};
+	int num[] = {'4', '3', '0', '7', '1'};
+
+	len1 = _strlen(s);
+	len2 = _strlen(alp);
+
+	for (i = 0; i < len1; i++)
+	{
+		for (j = 0; j < len2; j++)
+		{
+			if (s[i] != alp[j] && s[i] != (alp[j] - 32))
+			{
+				continue;
+			}
+			tmp = num[j];
+			s[i] = tmp;
+		}
+	}
+	return (s);
+}
