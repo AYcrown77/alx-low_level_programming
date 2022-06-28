ce75edc707b96c77088e63797d51caa15a703fe6
AYcrown77 <aycrown77@gmail.com>
2022-06-28 10:16:57 +0000

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string argument passed
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		count = count + 1;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * _strdup -  function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: pointer to the string passed as parameter
+ * Return: returns a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len, i;
+
+	len = _strlen(str);
+	ptr = (char *) malloc(sizeof(char) * len);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
