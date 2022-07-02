98aa922c5804e7e47dbff90b4a72e13d2c931c10
AYcrown77 <aycrown77@gmail.com>
2022-07-02 12:00:52 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *ptr1;
+	char *ptr1;
+	unsigned int i;
 
 	if (new_size == old_size)
 	{
@@ -24,6 +25,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr1 == NULL)
 	{
 		return (NULL);
+	}
+	if (old_size < new_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			ptr1[i] = ((char *)ptr)[i];
+		}
 	}
 
 	free(ptr);
