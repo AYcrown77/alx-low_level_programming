3383ca3daabbf17aa7c953bb077f25e156ed63b0
AYcrown77 <aycrown77@gmail.com>
2022-07-04 20:16:43 +0000

    0x0E-structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *ptr;
-	ptr = d;
 
-	ptr -> name = name;
-	ptr -> age = age;
-	ptr -> owner = owner;
+	ptr = d;
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
 }
