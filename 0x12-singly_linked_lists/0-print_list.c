d03c174e62272a6fc5d61e34c9c4d7d33b922fe5
AYcrown77 <aycrown77@gmail.com>
2022-07-18 17:59:37 +0000

    First push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,6 +9,11 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +22,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count++;
